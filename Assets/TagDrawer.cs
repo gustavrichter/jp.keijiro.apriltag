ae212978019fad7aa9f99f48781234a8d03c1fa2
gustavrichter <104318873+gustavrichter@users.noreply.github.com>
2023-10-28 18:38:52 +0200

    added CS

--- Assets/TagDrawer.cs
+++ Assets/TagDrawer.cs
@@ -4,9 +4,34 @@ sealed class TagDrawer : System.IDisposable
 {
     Mesh _mesh;
     Material _sharedMaterial;
-
+    Mesh[] axisMeshes;
+    Vector3[] axes;
+    Color[] axesColors;
+    Material[] axesMaterials;
+ 
     public TagDrawer(Material material)
     {
+        axisMeshes = new Mesh[3];
+        axes = new Vector3[3];
+        axesColors = new Color[3];
+        axesMaterials = new Material[3];
+
+        axes[0] = Vector3.right;
+        axes[1] = Vector3.up;
+        axes[2] = - Vector3.forward;
+
+        axesColors[0] = Color.red;
+        axesColors[1] = Color.green;
+        axesColors[2] = Color.blue;
+
+        axesMaterials[0] = Resources.Load("Wire 1") as Material;
+        axesMaterials[1] = Resources.Load("Wire 2") as Material;
+        axesMaterials[2] = Resources.Load("Wire 3") as Material;
+
+        for (int i = 0; i < 3; i++)
+        {
+            axisMeshes[i] = BuildAxisMesh(axes[i], axesColors[i]);
+        }
         _mesh = BuildMesh();
         _sharedMaterial = material;
     }
@@ -21,7 +46,11 @@ sealed class TagDrawer : System.IDisposable
     public void Draw(int id, Vector3 position, Quaternion rotation, float scale)
     {
         var xform = Matrix4x4.TRS(position, rotation, Vector3.one * scale);
-        Graphics.DrawMesh(_mesh, xform, _sharedMaterial, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            Graphics.DrawMesh(axisMeshes[i], xform, axesMaterials[i], 0);
+        }
+        //Graphics.DrawMesh(_mesh, xform, _sharedMaterial, 0);
     }
 
     static Mesh BuildMesh()
@@ -49,6 +78,47 @@ sealed class TagDrawer : System.IDisposable
         var mesh = new Mesh();
         mesh.vertices = vtx;
         mesh.SetIndices(idx, MeshTopology.Lines, 0);
+
+        return mesh;
+    }
+
+    static Mesh BuildAxisMesh(Vector3 direction, Color color)
+    {
+        var axisLength = 50.0f; // Length of the axes
+        var axisThickness = 0.1f; // Thickness of the axes
+
+        // Define the vertices for the axis
+        var vtx = new Vector3[]
+        {
+            Vector3.zero,
+            direction * axisLength
+        };
+
+        // Define the indices for the axis
+        var idx = new int[]
+        {
+            0, 1
+        };
+
+        var mesh = new Mesh();
+        mesh.vertices = vtx;
+        mesh.SetIndices(idx, MeshTopology.Lines, 0);
+
+        // Apply a line thickness to the axis by scaling it
+        Vector3[] scaledVertices = new Vector3[vtx.Length];
+        for (int i = 0; i < vtx.Length; i++)
+        {
+            scaledVertices[i] = Vector3.Scale(vtx[i], new Vector3(axisThickness, axisThickness, axisThickness));
+        }
+        mesh.vertices = scaledVertices;
+
+        // Assign a color to the axis
+        Color[] colors = new Color[vtx.Length];
+        for (int i = 0; i < vtx.Length; i++)
+        {
+            colors[i] = color;
+        }
+        mesh.colors = colors;
 
         return mesh;
     }
